50d8a3d12aaea2e06a30177d90a5ede4de696b47
jacobflee <jacoblee00000@gmail.com>
2020-10-17 19:43:20 -0700

    wowowowowow

---
+++ precache-manifest.41c82cb608faa1208f7a79652af1fc99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09baccf6ec2baed8ba3bcc9949b1a82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8933646cc1019aa9ec22",
+    "url": "/static/css/main.c314f22b.chunk.css"
+  },
+  {
+    "revision": "2c410d74d023c389ef6e",
+    "url": "/static/js/2.5bc6bbb1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5bc6bbb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8933646cc1019aa9ec22",
+    "url": "/static/js/main.3c1cb56f.chunk.js"
+  },
+  {
+    "revision": "d354045a71bdcddb577e",
+    "url": "/static/js/runtime-main.1c188b50.js"
+  }
+]);
